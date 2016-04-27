e1ee79a0517772ab9055c1ede8bdb08b0ca9e862
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-27 15:32:01 +0100

    connecté web socket avec la parite backend

--- server/main.js
+++ server/main.js
@@ -1,3 +1,14 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+app.use(express.static('app'));
+
+io.on('connection',function(socket){
+console.log("something connected to socket");
+    socket.emit("messages",["hello","hi there","how are you"]);
+    
+})
+server.listen(80);
